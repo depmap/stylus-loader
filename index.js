e4b18cf77cf07ddd8153a8fe199acb303d1f7bca
Henry Snopek <hhsnopek@gmail.com>
2017-04-04 20:57:12 -0400

    stablize

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 const fs = require('fs')
-const readline = require('readline')
 const path = require('path')
 const stylus = require('stylus')
 
 const keywords = ['@import', '@require']
 
 module.exports = {
+  meta: {
+    ext: 'styl',
+    outExt: 'css',
+    outDir: 'css'
+  },
   parse: (file, meta) => {
-    return new Promise((resolve, reject) => {
-      const deps = []
-      const rl = readline.createInterface({
-        input: fs.createReadStream(file, 'utf8')
-      })
-
-      rl.on('line', line => {
-        if (line.indexOf(keywords[0]) > -1 || line.indexOf(keywords[1]) > -1) {
-          let words = line.split(' ')
-          let file = words[words.length - 1]
-          deps.push(path.parse(file).name)
-        }
-      })
-        .on('error', err => reject(err))
-        .on('close', () => resolve(deps))
+    const deps = []
+    fs.readFileSync(file).toString().split('\n').forEach(line => {
+      if (line.indexOf(keywords[0]) > -1 || line.indexOf(keywords[1]) > -1) {
+        let words = line.split(' ')
+        let file = words[words.length - 1]
+        let meta = path.parse(file)
+        deps.push(`styl_${meta.name.replace(/'/g, '')}`)
+      }
     })
+
+    return deps
   },
   compile: {
-    string: (str, opts) => {
-      return new Promise((resolve, reject) => {
-        stylus.render(str, opts, (err, css) => {
-          if (err) reject(err)
-          resolve(css)
-        })
-      })
-    },
+    string: (str, opts) => { resolve(stylus.render(str, opts)) },
     file: (path, opts) => {
-      return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-          if (err) reject(err)
-          stylus.render(data, opts, (err, css) => {
-            if (err) reject(err)
-            resolve(css)
-          })
-        })
-      })
+      let css = stylus(fs.readFileSync(path).toString(), opts)
+        .set('filename', path)
+        .set('paths', [__dirname])
+        .render()
+      return css
     }
   }
 }
